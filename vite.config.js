c9dcbbe3344c909e8012cd8af4c91f4c486db170
Karthik1144 <gkarthik8461@gmail.com>
2025-10-07 12:48:37 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/s207/'
-});
-export default {
-  base: '/Git_Practice_Example2/',
-}
+  base: '/Git_Practice_Example2/', // 👈 IMPORTANT — match your repo name exactly
+})
